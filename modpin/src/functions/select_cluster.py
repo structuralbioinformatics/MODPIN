1d37ce7243560f891b7b8140a1284a95e9a24d48
Baldo Oliva <baldo.oliva@upf.edu>
2018-07-20 08:49:18 +0200

    fileExist in select_cluster and Exponential_Averaging_FEP will be defined in functions.py

--- modpin/src/functions/select_cluster.py
+++ modpin/src/functions/select_cluster.py
@@ -43,8 +43,6 @@ from SBI.structure.contacts import Complex
 from SBI.sequence import Sequence
 from SBI.structure import PDB
 
-def fileExist (file):
- return os.path.exists(file) and os.path.isfile(file)
 
 
 if len(sys.argv)==1:
